c38c92ef832bd6086353216d62b8fa78df524e6a
Cesar Gonzalez <gonzalezrivas912@gmail.com>
2016-02-02 22:09:24 -0800

    added search functionality

--- scripts.js
+++ scripts.js
@@ -8,13 +8,13 @@ var markers = [];
 function initMap(){
   var center = new google.maps.LatLng(34.0569172,-117.8239381);
   map = new google.maps.Map(document.getElementById('map'),{
-    center: center,zoom:11
+    center: center,zoom:13
   });
 
   request = {
     location: center,
     radius: 8047,
-    types: ['atm']
+    types: ['food']
   };
 
   infowindow = new google.maps.InfoWindow();
@@ -29,15 +29,14 @@ function initMap(){
       drawingModes: [
         google.maps.drawing.OverlayType.CIRCLE,
         google.maps.drawing.OverlayType.POLYGON,
-        google.maps.drawing.OverlayType.POLYLINE,
         google.maps.drawing.OverlayType.RECTANGLE
       ]
     },
     circleOptions: {
       fillColor: '#ffff00',
-      fillOpacity: .1,
-      strokeWeight: 1,
-      clickable: false,
+      fillOpacity: .2,
+      strokeWeight: .1,
+      clickable: true,
       editable: true,
       zIndex: 1
     }
@@ -45,10 +44,80 @@ function initMap(){
   drawingManager.setMap(map);
 
 
+
+  //Autocomplete functionality
+  var ac = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
+  google.maps.event.addListener(ac, 'place_changed', function(){
+    var place = ac.getPlace();
+    console.log(place.formatted_address);
+    console.log(place.url);
+    console.log(place.geometry.location);
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('autocomplete');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
+
+
+
+
+
   //User can search by region when they right click a section on the map. Radius is set to
   //5 miles to start and looks only for places containing atm for demostration.
 
-  google.maps.event.addListener(map, 'center_changed', function(event){
+  google.maps.event.addListener(map, 'rightclick', function(event){
     map.setCenter(event.latLng)
     clearResults(markers)
 
@@ -61,14 +130,6 @@ function initMap(){
 
   })
 
-  //Autocomplete functionality
-  var ac = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
-  google.maps.event.addListener(ac, 'place_changed', function(){
-    var place = ac.getPlace();
-    console.log(place.formatted_address);
-    console.log(place.url);
-    console.log(place.geometry.location);
-  });
 }
 
 //Displays results
@@ -103,5 +164,5 @@ function clear(markers){
   }
   markers = []
 }
-////Displays results when page is fully loaded
+//Displays results when page is fully loaded
 //google.maps.event.addDomListener(window,'load',initialize);
