7871747c6992638719e90d39ba34adaeeb3e8250
ihlee001 <iainhlee@gmail.com>
2016-03-03 21:50:53 -0800

    made sidebar grab places and added click events to them

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@ var markers = [];
 
 //  Displays map
 function initMap(){
+	var side_bar_html = "";
 	var center = new google.maps.LatLng(34.0569172,-117.8239381);
 	map = new google.maps.Map(document.getElementById('map'),{
 		center: center,zoom:13,
@@ -21,7 +22,6 @@ function initMap(){
 	});
 	
 	map.addListener('tilesloaded', function() {});
-
 
 	//create drawing manager at center
 	var drawingManager = create_draw_manager();
@@ -61,26 +61,22 @@ function initMap(){
 		places.forEach(function(place) {
 			var icon = create_icon(place);
 			if(drawn_shape == null){
-				push_marker(place, icon);
-				place_marker_info(place, bounds);
+				side_bar_html = push_place_sidebar(place, icon, bounds, side_bar_html);
 			}
 			else if(drawn_shape.type == google.maps.drawing.OverlayType.POLYGON){
 				if(google.maps.geometry.poly.containsLocation(place.geometry.location, drawn_shape.overlay)){
-					push_marker(place, icon);	
-					place_marker_info(place, bounds);
+					side_bar_html = push_place_sidebar(place, icon, bounds, side_bar_html);
 				}
 			}
 			else if(drawn_shape.type == google.maps.drawing.OverlayType.CIRCLE){
 				if(google.maps.geometry.spherical.computeDistanceBetween(place.geometry.location, 
 					drawn_shape.overlay.getCenter()) <= drawn_shape.overlay.getRadius()){
-					push_marker(place, icon);
-					place_marker_info(place, bounds);					
+					side_bar_html = push_place_sidebar(place, icon, bounds, side_bar_html);					
 				}
 			}
 			else if(drawn_shape.type == google.maps.drawing.OverlayType.RECTANGLE){
 				if(drawn_shape.overlay.getBounds().contains(place.geometry.location)){
-					push_marker(place, icon);	
-					place_marker_info(place, bounds);
+					side_bar_html = push_place_sidebar(place, icon, bounds, side_bar_html);
 				}
 			}
 			
@@ -92,7 +88,8 @@ function initMap(){
 		if(drawn_shape != null){
 			drawn_shape.overlay.setMap(null);
 			drawn_shape = null;
-		} 	
+		}
+		document.getElementById("side_bar").innerHTML = side_bar_html;
 	});
 
 	//Listens for any drawing events with circle rectangle or polygon
@@ -105,6 +102,13 @@ function initMap(){
 		drawn_shape = event;
 		drawingManager.setDrawingMode(null);
 	});
+}
+
+function push_place_sidebar(place, icon, bounds, side_bar_html){
+	push_marker(place, icon);
+	place_marker_info(place, bounds);
+	side_bar_html += '<a href="javascript:myclick(' + (markers.length-1) + ')">' + place.name + '<\/a><br>';
+	return side_bar_html
 }
 
 function create_icon(place){
@@ -195,6 +199,10 @@ function toggle_sidebar(){
 	document.getElementById("side_bar").style.display="inline";
 	document.getElementById("map").style.width="70%";
 	document.getElementById("map").style.left="30%";
+}
+
+function myclick(i){
+	google.maps.event.trigger(markers[i], 'click');
 }
 //Displays results when page is fully loaded
 //google.maps.event.addDomListener(window,'load',initialize);
